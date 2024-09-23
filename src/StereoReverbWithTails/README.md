dce2ce2ac6d8b0601a9f136e83e9aa3a5dc8618e
Cleveland Music Co. <code@clevelandmusicco.com>
2024-09-23 13:55:11 -0400

    Add StereoReverbWithTails example

---
+++ src/StereoReverbWithTails/README.md
@@ -0,0 +1,25 @@
+# StereoReverbWithTails
+
+Contributed by Cleveland Music Co. \<<code#clevelandmusicco.com>\>
+
+## Description
+
+A simple stereo reverb effect based on the DaisySP ReverbSC class. This is a "tails" implementation, meaning if the effect is disabled while processing audio through the reverb effect, the reverb tail is allowed to ring out. While the tail rings out, any new audio input will not be processed through the reverb effect until it is enabled again.
+
+Also note that _SWITCH 1_ selects between mono-to-stereo processing (UP) and true stereo-to-stereo processing (MIDDLE or DOWN). Mono-to-stereo is useful for mono instruments such as vintage synths, guitars, etc.
+
+### Controls
+
+| CONTROL | DESCRIPTION | NOTES |
+|-|-|-|
+| KNOB 1 | Reverb DECAY | Also called time or feedback |
+| KNOB 2 | Reverb LPF | Counter-clockwise to filter out more high frequencies from the tail |
+| KNOB 3 | Reverb SEND | Clockwise to send more of the dry signal to the reverb effect; works like a mixer FX send |
+| KNOB 4 | Reverb PRE-DELAY | 0 to 500ms |
+| KNOB 5 | Unused |  |
+| KNOB 6 | Unused |  |
+| SWITCH 1 | Unused | **UP** - mono-to-stereo mode (left input is copied to right)<br/>**MIDDLE** - true stereo mode<br/>**DOWN** - true stereo mode |
+| SWITCH 2 | Unused |  |
+| SWITCH 3 | Unused |  |
+| FOOTSWITCH 1 | Unused |  |
+| FOOTSWITCH 2 | Bypass | The bypassed signal is buffered |
---
+++ src/StereoReverbWithTails/stereo_reverb_with_tails.cpp
@@ -0,0 +1,135 @@
+// BasicSpringReverb for Hothouse DIY DSP Platform
+// A port of the petal/Verb example from the DaisyExamples repo
+// Copyright (C) 2024 Cleveland Music Co.
+// <code@clevelandmusicco.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "daisysp-lgpl.h"
+#include "daisysp.h"
+#include "hothouse.h"
+
+using clevelandmusicco::Hothouse;
+using daisy::AudioHandle;
+using daisy::Led;
+using daisy::Parameter;
+using daisysp::DelayLine;
+using daisysp::ReverbSc;
+
+Hothouse hw;
+Parameter v_time, v_freq, v_send, v_pre_delay;
+ReverbSc verb;
+
+static DelayLine<float, 48000> DSY_SDRAM_BSS pre_delay_l, pre_delay_r;
+
+Led led_bypass;
+bool bypass = true;
+bool tail_active = false;
+bool mono_to_stereo = false;
+
+float sample_rate;
+
+// Audio callback to process audio samples
+void AudioCallback(AudioHandle::InterleavingInputBuffer in,
+                   AudioHandle::InterleavingOutputBuffer out, size_t size) {
+  float dry_l, dry_r, wet_l, wet_r, send_l, send_r, pre_delayed_l,
+      pre_delayed_r;
+
+  hw.ProcessDigitalControls();
+  verb.SetFeedback(v_time.Process());
+  verb.SetLpFreq(v_freq.Process());
+  v_send.Process();
+  v_pre_delay.Process();
+
+  // Set pre-delay based on the knob value
+  float pre_delay_time = v_pre_delay.Value();
+  pre_delay_l.SetDelay(sample_rate * pre_delay_time);
+  pre_delay_r.SetDelay(sample_rate * pre_delay_time);
+
+  // Toggle mono-to-stereo mode
+  if (hw.switches[Hothouse::SWITCH_1_UP].Pressed()) {
+    mono_to_stereo = true;  // Mono-to-stereo mode is enabled
+  } else {
+    mono_to_stereo = false;  // Stereo-to-stereo mode is enabled
+  }
+
+  // Toggle bypass mode
+  if (hw.switches[Hothouse::FOOTSWITCH_2].RisingEdge()) {
+    bypass = !bypass;
+    if (bypass) tail_active = true;
+  }
+
+  for (size_t i = 0; i < size; i += 2) {
+    dry_l = in[i];
+    dry_r = in[i + 1];
+
+    // Mono-to-stereo logic
+    if (mono_to_stereo) {
+      dry_r = dry_l;
+    }
+
+    // Pre-delay processing
+    pre_delayed_l = pre_delay_l.Read();
+    pre_delay_l.Write(dry_l);
+    pre_delayed_r = pre_delay_r.Read();
+    pre_delay_r.Write(dry_r);
+
+    // Reverb processing
+    if (!bypass) {
+      send_l = pre_delayed_l * v_send.Value();
+      send_r = pre_delayed_r * v_send.Value();
+      verb.Process(send_l, send_r, &wet_l, &wet_r);
+      out[i] = dry_l + wet_l;
+      out[i + 1] = dry_r + wet_r;
+    } else if (tail_active) {
+      verb.Process(0.0f, 0.0f, &wet_l, &wet_r);
+      out[i] = dry_l + wet_l;
+      out[i + 1] = dry_r + wet_r;
+
+      // Check if tail has ended
+      if (wet_l == 0.0f && wet_r == 0.0f) tail_active = false;
+    } else {
+      out[i] = dry_l;
+      out[i + 1] = dry_r;
+    }
+  }
+}
+
+int main(void) {
+  hw.Init();
+  hw.SetAudioBlockSize(4);
+  sample_rate = hw.AudioSampleRate();
+
+  v_time.Init(hw.knobs[Hothouse::KNOB_1], 0.6f, 0.999f, Parameter::LOGARITHMIC);
+  v_freq.Init(hw.knobs[Hothouse::KNOB_2], 500.0f, 20000.0f,
+              Parameter::LOGARITHMIC);
+  v_send.Init(hw.knobs[Hothouse::KNOB_3], 0.0f, 1.0f, Parameter::LINEAR);
+  v_pre_delay.Init(hw.knobs[Hothouse::KNOB_4], 0.0f, 0.5f, Parameter::LINEAR);
+
+  verb.Init(sample_rate);
+
+  pre_delay_l.Init();
+  pre_delay_r.Init();
+
+  led_bypass.Init(hw.seed.GetPin(Hothouse::LED_2), false);
+
+  hw.StartAdc();
+  hw.StartAudio(AudioCallback);
+
+  while (1) {
+    hw.DelayMs(10);
+    led_bypass.Set(bypass ? 0.0f : 1.0f);
+    led_bypass.Update();
+  }
+}
